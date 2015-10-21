331c8a2c021beed49514a5e8afbc1a703fc4cd4d
Cahr8 <ne0919796>
2015-10-21 14:33:29 -0500

    Block Breaker
    Unity Game

---
+++ Assets/Script/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col)
+	{
+		GM.instance.LoseLife (); 
+	}
+
+}
---
+++ Assets/Script/Whatever.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whatever : MonoBehaviour {
+	
+	public GameObject brickParticle; 
+	
+	void OnCollisionEnter (Collision other)
+	{
+		Instantiate (brickParticle, transform.position, Quaternion.identity);
+		GM.instance.DestroyBrick();
+		Destroy(gameObject); 
+	}
+}
